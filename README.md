0bbffb499c1ad5ce35affc6e16320dab9797266e
auntu <ahmed.auntu@gmail.com>
2017-02-17 12:12:53 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# myproj
+# Initiate Framework
+Initial configuration with OVS install
+
+Install Github:
+
+:~$ sudo apt-get install git -y || sudo yum install -y git
+
+Download init_ovs file from   
+:~$ git clone https://github.com/auntu/init_ovs.git
+
+Run the init_ovs.sh bash script   
+:~$ chmod 755 init_ovs/init_ovs.sh  
+:~$ sudo su
+:~$ ./init_ovs/init_ovs.sh
+
+Initial Configuration is done!
---
+++ init_ovs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $(whoami)  != 'root' ]]; then
+	echo "Run as root";
+	exit;
+fi
+
+apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
+apt-get install sudo -y || yum install -y sudo
+apt-get install git -y || sudo yum install -y git
+
+if [[ $(grep -c 'stack ALL=(ALL) NOPASSWD: ALL' /etc/sudoers)  -eq 0 ]]; then 
+	echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+fi
+
+apt-get -y install openvswitch-switch openvswitch-common openvswitch-controller
+
+pushd /root/
+wget http://openvswitch.org/releases/openvswitch-2.3.3.tar.gz
+tar zxvf openvswitch-2.3.3.tar.gz
+rm -rf openvswitch-2.3.3.tar.gz
+
+cd openvswitch-2.3.3/
+./configure --prefix=/usr --with-linux=/lib/modules/`uname -r`/build
+make && make install && make modules_install
+rmmod openvswitch && depmod -a
+
+popd
+/etc/init.d/openvswitch-controller stop
+update-rc.d openvswitch-controller disable
+/etc/init.d/openvswitch-switch restart
+
+ovs-vsctl show
