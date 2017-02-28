de444c0362f22650d639dced590f8c359f31bd85
auntu <ahmed.auntu@gmail.com>
2017-02-28 14:45:16 +0100

    Update ryumininet.sh

--- ryumininet.sh
+++ ryumininet.sh
@@ -7,7 +7,7 @@ sudo pip install setuptools --upgrade
 
 echo "Step 2: Clone RYU from git Repo and Installing Ryu"
 git clone --depth=1 https://github.com/auntu/ryu.git
-sudo python ./ryu/setup.py install
+cd /home/stack/ryu && sudo python ./ryu/setup.py install
 
 echo "Step 3. Install and Update python packages"
 sudo pip install six --upgrade
