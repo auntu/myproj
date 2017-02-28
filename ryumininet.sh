a53cb5697d2a05f1db53d611b7c9b2acf7846a4b
auntu <ahmed.auntu@gmail.com>
2017-02-28 11:45:18 +0100

    Add files via upload

---
+++ ryumininet.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Step 1: Install python tools and packages"
+sudo apt-get -y install python-pip python-dev python3-pip python3-dev build-essential
+sudo apt-get -y install python-eventlet python-routes python-webob python-paramiko python-netaddr
+sudo pip install setuptools --upgrade
+
+echo "Step 2: Clone RYU from git Repo and Installing Ryu"
+git clone --depth=1 https://github.com/auntu/ryu.git
+#git clone https://github.com/auntu/ryu.git -b v3.17 ryu
+cd ryu; git checkout -b v3.17 v3.17
+sudo python ./setup.py install
+cd~
+
+echo "Step 3. Install and Update python packages"
+sudo pip install six --upgrade
+#sudo pip install -r ryu/tools/pip-requires
+sudo pip install oslo.config msgpack-python
+sudo pip install eventlet --upgrade
+sudo pip install tinyrpc
+
+echo "Step 4. Test ryu-manager"
+cd /home/stack/ryu && ./bin/ryu-manager --version
+cd~
+
+#echo "Step 5. Installing Mininet"
+#git clone git://github.com/mininet/mininet
+#cd mininet; git checkout -b 2.2.1 2.2.1
+#./util/install.sh -nf3
+#cd~
+
+#echo "Step 6. Testing Mininet"
+#sudo mn --test pingall
+#sudo mn -c
